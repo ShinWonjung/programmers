9f203792fb2d308d26c95cdd78197ea15e457cb1
원정 신 <dnjswjd1204@naver.com>
2021-08-24 21:11:22 +0900

    commit

---
+++ middlechar/middlechar/middlechar.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int mid = s.size() / 2;
+    if (s.length() % 2 == 0)
+    {
+        answer += s[mid - 1];
+        answer += s[mid];
+    }
+    else
+        answer += s[mid];
+    return answer;
+}
