195a1bde409a75bee7c95f590dc16c4143e3554a
원정 신 <dnjswjd1204@naver.com>
2021-09-09 19:40:24 +0900

    commit

--- maxsusic/maxsusic/소스.cpp
+++ maxsusic/maxsusic/소스.cpp
@@ -1,44 +1,95 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <cstdlib>
 
 using namespace std;
+vector<pair<string, char>> priority{ {"*",' '},{"+",' '},{"-",' '} };
+vector<string> pr{ "012","021","102","120","201","210" };
 
-vector<string> pluspostfix(vector<string> vec)
+vector<string> postfix(vector<string> vec)
 {
     vector<string>v;
-    stack<string>s;
+    stack<pair<string, char>>s;
     for (int i = 0; i < vec.size(); i++)
     {
-        if (vec[i] == "*") s.push(vec[i]);
-        if (vec[i] == "+")
+        if (vec[i] == "*")
         {
-            if (s.empty()) s.push(vec[i]);
-            else if (s.top() != "-")
+            if (s.empty()) s.push(make_pair(vec[i], priority[0].second));
+            else if (s.top().second >= priority[0].second)
             {
-                while (!s.empty()) 
+                while (!s.empty() && s.top().second >= priority[0].second)
                 {
-                    v.push_back(s.top());
+                    v.push_back(s.top().first);
                     s.pop();
                 }
-                s.push(vec[i]);
+                s.push(make_pair(vec[i], priority[0].second));
             }
+            else
+                s.push(make_pair(vec[i], priority[0].second));
         }
-        if (vec[i] == "-")
+        else if (vec[i] == "+")
         {
-            while (!s.empty())
+            if (s.empty()) s.push(make_pair(vec[i], priority[1].second));
+            else if (s.top().second >= priority[1].second)
             {
-                v.push_back(s.top());
-                s.pop();
+                while (!s.empty() && s.top().second >= priority[1].second)
+                {
+                    v.push_back(s.top().first);
+                    s.pop();
+                }
+                s.push(make_pair(vec[i], priority[1].second));
             }
-            s.push(vec[i]);
+            else
+                s.push(make_pair(vec[i], priority[1].second));
+        }
+        else if (vec[i] == "-")
+        {
+            if (s.empty()) s.push(make_pair(vec[i], priority[2].second));
+            else if (s.top().second >= priority[2].second)
+            {
+                while (!s.empty() && s.top().second >= priority[2].second)
+                {
+                    v.push_back(s.top().first);
+                    s.pop();
+                }
+                s.push(make_pair(vec[i], priority[2].second));
+            }
+            else
+                s.push(make_pair(vec[i], priority[2].second));
+        }
+        else
+            v.push_back(vec[i]);
+    }
+    while (!s.empty())
+    {
+        v.push_back(s.top().first);
+        s.pop();
+    }
+    return v;
+}
+long long solve(vector<string> vec)
+{
+    long long answer = 0;
+    stack<long long>s;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (isdigit(vec[i][0]))
+            s.push(stoi(vec[i]));
+        else
+        {
+            answer = s.top();
+            s.pop();
+            if (vec[i] == "*") answer = s.top() * answer;
+            else if (vec[i] == "+") answer = s.top() + answer;
+            else if (vec[i] == "-") answer = s.top() - answer;
+            s.pop();
+            s.push(answer);
         }
     }
+    return s.top();
 }
-
 long long solution(string expression) {
-    int plus = 0;
-    int minus = 0;
     vector<string> vec;
     string s = "";
     for (int i = 0; i < expression.length(); i++)
@@ -47,15 +98,25 @@ long long solution(string expression) {
             s += expression[i];
         else
         {
-            if (expression[i] == '+') plus++;
-            else if (expression[i] == '-') minus++;
             vec.push_back(s);
-            i++;
             s = expression[i];
             vec.push_back(s);
             s = "";
         }
     }
+    vec.push_back(s);
     long long answer = 0;
-    return answer;
+    long long max = 0;
+    
+    for (int i = 0; i < pr.size(); i++)
+    {
+        priority[0].second = pr[i][0];
+        priority[1].second = pr[i][1];
+        priority[2].second = pr[i][2];
+        vector<string>temp = postfix(vec);
+        answer = abs(solve(temp));
+        if (max < answer) max = answer;
+    }
+
+    return max;
 }
