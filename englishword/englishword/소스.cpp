8ed91d5abf34ae28b4cbb1904d182aef4711c3b2
원정 신 <dnjswjd1204@naver.com>
2021-09-10 22:24:16 +0900

    commit

--- englishword/englishword/소스.cpp
+++ englishword/englishword/소스.cpp
@@ -1,54 +1,25 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
+#include <unordered_set>
 
 using namespace std;
-
-bool comp(pair<string, int> p1, pair<string, int>p2)
-{
-    if (p1.first == p2.first)
-        return p1.second < p2.second;
-    return p1.first < p2.first;
-}
 
 vector<int> solution(int n, vector<string> words) {
     vector<int> answer(2, 0);
 
-    vector<pair<string, int>>sorted;
-    for (int i = 0; i < words.size(); i++)
-    {
-        sorted.push_back(make_pair(words[i], i));
-    }
-    sort(sorted.begin(), sorted.end());
-
-    int min = 101;
-    for (int i = 1; i < sorted.size(); i++)
-    {
-        if (sorted[i].first == sorted[i - 1].first)
-        {
-            if (sorted[i].second < min)
-                min = sorted[i].second;
-        }
-    }
-
+    unordered_set<string>set;
+    set.insert(words[0]);
     for (int i = 1; i < words.size(); i++)
     {
-        if (words[i - 1].back() != words[i].front())
+        set.insert(words[i]);
+        if (words[i - 1].back() != words[i].front() ||
+            set.size() - 1 != i)
         {
-            if (min > i) min = i;
+            answer[0] = i % n + 1;
+            answer[1] = i / n + 1;
             break;
         }
     }
-    if (min != 101)
-    {
-        answer[0] = min % n + 1;
-        answer[1] = min / n + 1;
-    }
 
     return answer;
-}
-int main()
-{
-    solution(3, { "tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank" });
 }
