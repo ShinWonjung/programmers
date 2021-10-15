962aadb3764f2440376c00823f9d1cf87992f8bd
ShinWonjung <dnjswjd1204@gmail.com>
2021-10-15 21:53:28 +0900

    commit

--- ground/ground/소스.cpp
+++ ground/ground/소스.cpp
@@ -1,22 +1,18 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int d[100000];
-
 int solution(vector<vector<int> > land)
 {
     int answer = 0;
-    for (int i = 0; i < land.size(); i++)
+    for (int i = 1; i < land.size(); i++)
     {
-        
+        land[i][0] += max(land[i - 1][1], max(land[i - 1][2], land[i - 1][3]));
+        land[i][1] += max(land[i - 1][0], max(land[i - 1][2], land[i - 1][3]));
+        land[i][2] += max(land[i - 1][0], max(land[i - 1][1], land[i - 1][3]));
+        land[i][3] += max(land[i - 1][0], max(land[i - 1][1], land[i - 1][2]));
     }
-
+    answer = max(land.back()[0], max(land.back()[1], max(land.back()[2], land.back()[3])));
     return answer;
-}
-int main() 
-{
-    cout<<solution({ {4,3,2,1},{2,2,2,1},{6,6,6,4},{8,7,6,5} });
 }
